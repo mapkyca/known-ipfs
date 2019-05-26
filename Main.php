bc04c98b1d007de8c64a1d74d971e3a165facd25
Marcus Povey <marcus@marcus-povey.co.uk>
2019-05-26 17:26:59 +0100

    Adding composer resources & removing deprecated functions

--- Main.php
+++ Main.php
@@ -8,7 +8,7 @@ namespace IdnoPlugins\IPFS {
         function registerPages()
         {
 	    // Register admin settings
-	    \Idno\Core\site()->addPageHandler('admin/ipfs', '\IdnoPlugins\IPFS\Pages\Admin');
+	    \Idno\Core\Idno::site()->routes()->addRoute('admin/ipfs', '\IdnoPlugins\IPFS\Pages\Admin');
 
 	    // Add menu items to account & administration screens
 	    \Idno\Core\site()->template()->extendTemplate('admin/menu/items', 'admin/ipfs/menu');
@@ -30,9 +30,9 @@ namespace IdnoPlugins\IPFS {
 
 	    if ($config->IPFS['use_cdn'] == 'Yes') { 
 		// Override the image proxy
-		\Idno\Core\Idno::site()->addPageHandler('/service/web/imageproxy/([^\/]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true);
-		\Idno\Core\Idno::site()->addPageHandler('/service/web/imageproxy/([^\/]+)/([0-9]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true); // With scale
-		\Idno\Core\Idno::site()->addPageHandler('/service/web/imageproxy/([^\/]+)/([0-9]+)/([^\/]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true); // With scale, with transform
+		\Idno\Core\Idno::site()->routes()->addRoute('/service/web/imageproxy/([^\/]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true);
+		\Idno\Core\Idno::site()->routes()->addRoute('/service/web/imageproxy/([^\/]+)/([0-9]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true); // With scale
+		\Idno\Core\Idno::site()->routes()->addRoute('/service/web/imageproxy/([^\/]+)/([0-9]+)/([^\/]+)/?', '\IdnoPlugins\IPFS\Pages\Service\ImageProxy', true); // With scale, with transform
 	    }
 	}
 
